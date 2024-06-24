afb5e4fcae43667df6ec715057889211f5d25e9d
Thor Brink <thor.brink@helsingborg.se>
2024-06-24 09:40:19 +0200

    feat: update_field (#3)

--- src/AcfService.php
+++ src/AcfService.php
@@ -5,14 +5,15 @@ namespace AcfService;
 use AcfService\Contracts;
 
 interface AcfService extends
-    Contracts\AddOptionsPage,
     Contracts\AddLocalFieldGroup,
+    Contracts\AddOptionsPage,
     Contracts\EnqueueUploader,
     Contracts\Form,
     Contracts\FormHead,
     Contracts\GetField,
     Contracts\GetFieldGroups,
     Contracts\GetFields,
-    Contracts\RenderFieldSetting
+    Contracts\RenderFieldSetting,
+    Contracts\UpdateField
 {
 }
